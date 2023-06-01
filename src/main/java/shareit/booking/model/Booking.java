dc20cbfb8eb15443c6b8428a071f14ea5ef857d9
Кирилл <Stockmanrat@yandex.ru>
2023-06-02 01:04:16 +0300

    вместо @DATA использованы стандартные сеттеры и геттеры, поставлен лимит в requests таблице БД. убран отлов исключения лишний

--- src/main/java/shareit/booking/model/Booking.java
+++ src/main/java/shareit/booking/model/Booking.java
@@ -2,17 +2,16 @@ package shareit.booking.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import shareit.item.model.Item;
 import shareit.user.model.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "bookings")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -28,6 +27,54 @@ public class Booking {
     @Column(name = "end_date", nullable = false)
     private LocalDateTime end;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setBooker(User booker) {
+        this.booker = booker;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public User getBooker() {
+        return booker;
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id", nullable = false)
     private Item item;
@@ -38,4 +85,30 @@ public class Booking {
 
     @Enumerated(EnumType.STRING)
     private BookingStatus status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Booking booking = (Booking) o;
+
+        if (!Objects.equals(id, booking.id)) return false;
+        if (!Objects.equals(start, booking.start)) return false;
+        if (!Objects.equals(end, booking.end)) return false;
+        if (!Objects.equals(item, booking.item)) return false;
+        if (!Objects.equals(booker, booking.booker)) return false;
+        return status == booking.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (start != null ? start.hashCode() : 0);
+        result = 31 * result + (end != null ? end.hashCode() : 0);
+        result = 31 * result + (item != null ? item.hashCode() : 0);
+        result = 31 * result + (booker != null ? booker.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/shareit/item/comment/model/Comment.java
+++ src/main/java/shareit/item/comment/model/Comment.java
@@ -2,21 +2,60 @@ package shareit.item.comment.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import shareit.item.model.Item;
 import shareit.user.model.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "comments")
 @Builder
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Data
 public class Comment {
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +74,28 @@ public class Comment {
 
     @Column(name = "created", nullable = false)
     private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment = (Comment) o;
+
+        if (!Objects.equals(id, comment.id)) return false;
+        if (!Objects.equals(text, comment.text)) return false;
+        if (!Objects.equals(item, comment.item)) return false;
+        if (!Objects.equals(author, comment.author)) return false;
+        return Objects.equals(created, comment.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + (item != null ? item.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
+    }
 }
